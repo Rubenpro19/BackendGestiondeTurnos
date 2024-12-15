baac248afab991b7c6662b162fd14b2abcc3a9c1
Rubenpro19 <dariomera911@gmail.com>
2024-12-14 21:09:21 -0500

    Waos

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-/* Route::view('/', 'welcome');
+Route::view('/', 'welcome');
 Route::get('/usuario', [UsuarioController::class, 'index'])->name('usuario.index');
 Route::get('/usuario/{id}',[UsuarioController::class,'registro_unico'])->name('usuario.show');
 
 Route::post('/usuario3', [UsuarioController::class, 'store']);
- */

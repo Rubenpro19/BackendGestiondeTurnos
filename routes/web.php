d6905b83ea427f61a661e47659f3db9451562047
Rubenpro19 <dariomera911@gmail.com>
2024-12-14 21:39:29 -0500

    xd

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,3 @@
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-Route::get('/usuario', [UsuarioController::class, 'index'])->name('usuario.index');
-Route::get('/usuario/{id}',[UsuarioController::class,'registro_unico'])->name('usuario.show');
-
-Route::post('/usuario3', [UsuarioController::class, 'store']);
